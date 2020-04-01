5997dfd49b01d0e2ec817cdb5addbf00e29f0d8e
samuel olarte <samuelolarte300@gmail.com>
2020-03-31 22:10:39 -0300

    cuarto commits

--- js/sticky.js
+++ js/sticky.js
@@ -1,12 +1,25 @@
 const img = document.querySelector('.boton_modal');
 const offset = img.getBoundingClientRect();
 
-window.addEventListener('scroll', function() {
-  if (window.pageYOffset > offset.top) {
+window.addEventListener('scroll', function() { 
+    console.log(offset.top);
+
+    console.log(window.pageYOffset);
+
+
+  if (window.pageYOffset > 262.8) { 
    img.style.position = 'fixed';
-    img.style.top = 0;
+    img.style.top = '56px';
+    
   } else {
-    img.style.position = 'relative';
+    img.style.position = 'absolute';
     img.style.top = '';
   }
+
+//   if (window.pageYOffset > offset.top + 10) {
+//     img.style.position = 'fixed';
+//      img.style.top = 5;
+     
+//    }
 });
+
