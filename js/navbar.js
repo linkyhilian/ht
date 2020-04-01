fbcff97cdac06f7da02c0e9328de35b3c712dabd
samuel olarte <samuelolarte300@gmail.com>
2020-04-01 00:16:15 -0300

    seleccion en mobile

--- js/navbar.js
+++ js/navbar.js
@@ -12,3 +12,33 @@ $(function() {
   });
 
 });
+
+$(function() {
+  
+  // elementos de la lista
+  var menues = $(".nav-sidebar .sidebar_item"); 
+
+  // manejador de click sobre todos los elementos
+  menues.click(function() {
+     // eliminamos active de todos los elementos
+     menues.removeClass("active_border");
+     // activamos el elemento clicado.
+     $(this).addClass("active_border");
+  });
+
+});
+
+$(function() {
+  
+  // elementos de la lista
+  var menues = $(".nav-sidebar a"); 
+
+  // manejador de click sobre todos los elementos
+  menues.click(function() {
+     // eliminamos active de todos los elementos
+     menues.removeClass("active");
+     // activamos el elemento clicado.
+     $(this).addClass("active");
+  });
+
+});
