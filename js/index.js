52e9d8fe478078fa80181a132f21af239dfd9ec0
samuel olarte <samuelolarte300@gmail.com>
2020-03-31 22:54:58 -0300

    button_secret

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
 var bars_icon = document.querySelector('.bars_icon');
 var menu_side = document.querySelector('.menu_side');
 var menu_background = document.querySelector('.menu_side_background');
+const img = document.querySelector('.boton_modal');
+const offset = img.getBoundingClientRect();
+
 
 
 function sidebarToggle(){
     menu_side.classList.toggle('menu_side_active');
     menu_background.classList.toggle('menu_side_background_active');
-
+    img.classList.toggle('none')
+    
+   
 }
 
 
@@ -19,4 +24,20 @@ menu_background.addEventListener('click', () =>{
 
 
 
+
+window.addEventListener('scroll', function() { 
+    
+if(!menu_side.classList.contains('menu_side_active')){
+   console.log("ejecutando")
+  if (window.pageYOffset > 250) { 
+   img.style.position = 'fixed';
+    img.style.top = '70px';
+    
+  } else {
+    img.style.position = 'absolute';
+    img.style.top = '';
+  }
+
+}
+});
 
--- js/sticky.js
+++ js/sticky.js
@@ -2,24 +2,18 @@ const img = document.querySelector('.boton_modal');
 const offset = img.getBoundingClientRect();
 
 window.addEventListener('scroll', function() { 
-    console.log(offset.top);
+   
+ 
 
-    console.log(window.pageYOffset);
-
-
-  if (window.pageYOffset > 262.8) { 
+  if (window.pageYOffset > 250) { 
    img.style.position = 'fixed';
-    img.style.top = '56px';
+    img.style.top = '70px';
     
   } else {
     img.style.position = 'absolute';
     img.style.top = '';
   }
 
-//   if (window.pageYOffset > offset.top + 10) {
-//     img.style.position = 'fixed';
-//      img.style.top = 5;
-     
-//    }
+
 });
 
