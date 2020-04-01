104185abf8808c7d7454f2ba2173b2ba3c6621df
samuel olarte <samuelolarte300@gmail.com>
2020-03-31 23:56:42 -0300

    select nav active

---
+++ js/navbar.js
@@ -0,0 +1,14 @@
+$(function() {
+  
+  // elementos de la lista
+  var menues = $("nav li"); 
+
+  // manejador de click sobre todos los elementos
+  menues.click(function() {
+     // eliminamos active de todos los elementos
+     menues.removeClass("active");
+     // activamos el elemento clicado.
+     $(this).addClass("active");
+  });
+
+});
