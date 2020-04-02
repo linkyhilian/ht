280ae92cbc3a0b8742d9d1c7178029f9656c37c0
samuel olarte <samuelolarte300@gmail.com>
2020-04-02 03:40:20 -0300

    Gran Fix / Order Folder

--- js/index.js
+++ js/index.js
@@ -1,35 +1,37 @@
 var bars_icon = document.querySelector('.bars_icon');
 var menu_side = document.querySelector('.menu_side');
 var menu_background = document.querySelector('.menu_side_background');
+var sidebar = document.querySelector('.nav-sidebar');
 var img = document.querySelector('.boton_modal');
-// const offset = img.getBoundingClientRect();
-
-document.querySelector('body').addEventListener('click', function(event){
-  // event.target 
-
-  console.log(event.target)
-
-})
 
 
 
+// document.querySelector('body').addEventListener('click', function(event){
+//   // event.target 
+//   console.log(event.target)
+// })
+
+
+
+// sidebar
 function sidebarToggle(){
     menu_side.classList.toggle('menu_side_active');
     menu_background.classList.toggle('menu_side_background_active');
+    sidebar.classList.toggle('none')
     img.classList.toggle('none')
     
    
 }
-
-
 menu_background.addEventListener('click', () =>{
 
     sidebarToggle();
-    
 });
 
+
+
+
+// Sticky
 window.addEventListener('scroll', function() { 
-    
 if(!menu_side.classList.contains('menu_side_active')){
   if (window.pageYOffset > 136) { 
    img.style.position = 'fixed';
