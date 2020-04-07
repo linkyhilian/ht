13c792b29de0799df8bdf355b7af4fcc794042e3
samuel olarte <samuelolarte300@gmail.com>
2020-04-07 17:36:53 -0300

    scrolldown nav mod

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ var sidebar = document.querySelector('.nav-sidebar');
 var img = document.querySelector('.boton_modal');
 
 
+
 // sidebar
 function sidebarToggle(){
     menu_side.classList.toggle('menu_side_active');
@@ -36,21 +37,14 @@ if(!menu_side.classList.contains('menu_side_active')){
 });
 
 // navbar 
-window.addEventListener('scroll', function() { 
-  if(!menu_side.classList.contains('menu_side_active')){
-    if (window.pageYOffset > 136) { 
-     img.style.position = 'fixed';
-      img.style.top = '180px';
-      
-    } else {
-      img.style.position = 'absolute';
-      img.style.top = '';
-    }
-  
-  }
-  });
+window.onscroll = function() {scrollFunction()};
 
-  $("#test").on("click",function(event){
-    event.preventDefault();
-    console.log("hola");
- });
+function scrollFunction() {
+  if (document.body.scrollTop > 522 || document.documentElement.scrollTop > 522) {
+    document.getElementById("navbar").style.height = "100px";
+    document.getElementById("nav_container").style.height="100px"
+  } else {
+    document.getElementById("navbar").style.height = "142px";
+    document.getElementById("nav_container").style.height="142px"
+  }
+}
