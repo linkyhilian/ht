609b75bd7edf31254e97643b9f45c373a4cb7187
samuel olarte <samuelolarte300@gmail.com>
2020-04-14 12:43:33 -0300

    fix

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,22 +1,22 @@
-let modal = document.getElementById('modal');
-let flex= document.getElementById('flex');
-let abrir = document.getElementById('open_modal');
-let cerrar = document.getElementById('close');
+let sidebar_modal = document.getElementById('sidebar_modal');
+let flex_sidebar = document.getElementById('flex_sidebar');
+let open_sidebar = document.getElementById('open_sidebar');
+let close_sidebar = document.getElementById('close_sidebar');
 var img = document.querySelector('.boton_modal');
 
-abrir.addEventListener('click',function(){
-  modal.style.display='block'
+open_sidebar.addEventListener('click',function(){
+  sidebar_modal.style.display='block'
   img.classList.toggle('none')
 })
 
-cerrar.addEventListener('click',function(){
-  modal.style.display='none'
+close_sidebar.addEventListener('click',function(){
+  sidebar_modal.style.display='none'
   img.classList.toggle('none')
 })
 
 window.addEventListener('click',function(e){
-  if(e.target == flex){
-    modal.style.display='none'
+  if(e.target == flex_sidebar){
+    sidebar_modal.style.display='none'
     img.classList.toggle('none')
   }
 })
